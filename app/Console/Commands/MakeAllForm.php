00aa80d31e5b124919febfbce0d78754df6385c0
Nykcolas <nicharaujo@gmail.com>
2022-11-21 07:28:49 -0300

    criar colunas

--- app/Console/Commands/MakeAllForm.php
+++ app/Console/Commands/MakeAllForm.php
@@ -20,32 +20,48 @@ class MakeAllForm extends Command
     {
         $this->callCommandMake();
         $this->createANewLine();
+        if ($this->confirm('Você deseja criar as colunas da tabela "'.$this->argument('name').'" agora?
+        OBS.: Não recomendado em caso de uso de FK')) {
+            $this->comment("Crie um objeto com as clunas desejadas, seguindo o senguinte padrão:
+
+            {
+                'nome_coluna':
+                    {
+                        'type':'varchar'
+                        'default':null
+                        'length':50
+                    }
+                }"
+        );
+            $array_colums = $this->ask("");
+            dd(json_decode($array_colums));
+        }
     }
 
     public function createANewLine()
     {
-        File::append('resources/views/layouts/tabsForm.blade.php', "\n<a class='dropdown-item' href='{{route(\"".$this->argument('name')."\")}}'>".ucfirst($this->argument('name'))."</a>");
-        File::replaceInFile("//Forms", "//Forms\nRoute::apiResource('".$this->argument('name')."', \App\Http\Controllers\\".ucfirst($this->argument('name'))."Controller::class);", 'routes/api.php');
-        File::replaceInFile("//Forms", "//Forms\n    Route::get('/".$this->argument('name')."', function() {return view('app\\".$this->argument('name')."');})->name('".$this->argument('name')."');", 'routes/web.php');
-        File::replaceInFile("//Form", "//Form\nVue.component('".ucfirst($this->argument('name'))."', require('./components/app/".ucfirst($this->argument('name')).".vue').default);", 'resources/js/app.js');
+        // File::append('resources/views/layouts/tabsForm.blade.php', "\n<a class='dropdown-item' href='{{route(\"".$this->argument('name')."\")}}'>".ucfirst($this->argument('name'))."</a>");
+        // File::replaceInFile("//Forms", "//Forms\nRoute::apiResource('".$this->argument('name')."', \App\Http\Controllers\\".ucfirst($this->argument('name'))."Controller::class);", 'routes/api.php');
+        // File::replaceInFile("//Forms", "//Forms\n    Route::get('/".$this->argument('name')."', function() {return view('app\\".$this->argument('name')."');})->name('".$this->argument('name')."');", 'routes/web.php');
+        // File::replaceInFile("//Form", "//Form\nVue.component('".ucfirst($this->argument('name'))."', require('./components/app/".ucfirst($this->argument('name')).".vue').default);", 'resources/js/app.js');
     }
 
     public function callCommandMake()
     {
         $commands = [
-            'form'=>"created Successfully a new Form controller",
-            'formModel'=>"created Successfully a new Form Model",
-            'formUpdate'=>"created Successfully a new Form Update",
-            'formStore'=>"created Successfully a new Form Store",
-            'formMigration'=>"created Successfully a new Form Migration",
-            'formSeeder'=>"created Successfully a new Form Seeder",
-            'formFactory'=>"created Successfully a new Form Factory",
-            'formView'=>"created Successfully a new Form View",
-            'formVue'=>"created Successfully a new Form Vue"
+            'form'=>"Um novo arquivo controller para Form foi criado com sucesso",
+            'formModel'=>"Um novo arquivo Model para Form foi criado com sucesso",
+            'formUpdate'=>"Um novo arquivo Update para Form foi criado com sucesso",
+            'formStore'=>"Um novo arquivo Store para Form foi criado com sucesso",
+            'formMigration'=>"Um novo arquivo Migration para Form foi criado com sucesso",
+            'formSeeder'=>"Um novo arquivo Seeder para Form foi criado com sucesso",
+            'formFactory'=>"Um novo arquivo Factory para Form foi criado com sucesso",
+            'formView'=>"Um novo arquivo View para Form foi criado com sucesso",
+            'formVue'=>"Um novo arquivo Vue para Form foi criado com sucesso",
         ];
 
         foreach ($commands as $key => $value) {
-            Artisan::call('make:'.$key, ['name'=>$this->argument('name')]);
+            // Artisan::call('make:'.$key, ['name'=>$this->argument('name')]);
             $this->info($value);
         }
     }
